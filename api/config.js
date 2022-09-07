d270a0beb8f18940bb021e2d3b72da12cd1b51e3
aaqidmasoodi <aaqidmasoodi@icloud.com>
2022-09-07 22:42:31 +0530

    updated url

--- api/config.js
+++ api/config.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const BASE_URL = 'https://cukbrs.herokuapp.com/api';
 
 // export const LIVE_LOCATION_SOCKET_URL = 'ws://10.0.2.2:3000/api/buses/live/'
-export const LIVE_LOCATION_SOCKET_URL = 'ws://cukbrs.herokuapp.com/api/buses/live/'
+export const LIVE_LOCATION_SOCKET_URL = 'http://cukbrs.herokuapp.com/api/buses/live/'
 
 export default axios.create({
     baseURL: BASE_URL,
